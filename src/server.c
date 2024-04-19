84f4f4474cd3a7ebb77d103656df44ea61cc1c3e
Emre D <emrededemoglu149@gmail.com>
2024-04-19 14:12:01 +0200

    server msg handler

--- src/server.c
+++ src/server.c
@@ -6,35 +6,34 @@
 /*   By: ededemog <ededemog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 22:55:07 by ededemog          #+#    #+#             */
-/*   Updated: 2024/04/17 16:11:47 by ededemog         ###   ########.fr       */
+/*   Updated: 2024/04/19 14:11:52 by ededemog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/mini_talk.h"
 #include "../inc/ft_printf/ft_printf.h"
 
-void    handler(int signum)
+void    signal_handler(int sig)
 {
-    if (signum == SIGUSR1)
-        ft_printf("Received !\n");
+    static unsigned char actual_char;
+    static int  bit_position;
+
+    actual_char |= (sig == SIGUSR1);
+    bit_position++;
+    if (bit_position == 8)
+    {
+        if (actual_char == '\0')
+            ft_printf("\n");
+        else
+            ft_printf("%c", actual_char);
+        bit_position = 0;
+        actual_char = 0;
+    }
+    else
+        actual_char <<= 1;
 }
 
 int main(void)
 {
-    struct sigaction sa;
-    sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = handler;
-    sigemptyset(&sa.sa_mask);
 
-    sigaction(SIGUSR1, &sa, NULL);
-    sigaction(SIGUSR2, &sa, NULL);
-    
-    pid_t pid = getpid();
-    ft_printf("PID: %ld\n", (long)pid);
-    
-    ft_printf("Waiting for messages...\n");
-    while (1)
-    {
-        sleep(1);
-    }
 }
